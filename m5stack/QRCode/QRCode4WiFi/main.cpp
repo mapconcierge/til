94a57f9f252003732c731701582943027e02ecfa
Taichi FURUHASHI <mapconcierge@gmail.com>
2023-05-04 23:30:45 +0900

    Update main.cpp
    https://www.1ft-seabass.jp/memo/2022/11/05/m5stack-simple-qrcode-and-button/ を参考に M5Core2用にライブラリだけ修正

--- m5stack/QRCode/QRCode4WiFi/main.cpp
+++ m5stack/QRCode/QRCode4WiFi/main.cpp
@@ -1,44 +1,37 @@
 #include <M5Core2.h>
-#include <WiFi.h>
-#include <qrcode.h>
-
-// 以下にSSID、パスワード、暗号化方式を設定してください。
-const char* SSID = "your_SSID_here";
-const char* password = "your_password_here";
-const char* encryptiontype = "WPA2"; // 例: "WPA2", "WPA", "WEP", "OPEN"
-
+  
 void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(3);
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.setTextColor(TFT_WHITE);
-
-  String qrData = "WIFI:S:" + String(SSID) + ";T:" + String(encryptiontype) + ";P:" + String(password) + ";;";
-  displayQRCode(qrData);
+  
+  // ディスプレイとシリアルとON
+  M5.begin(true, false, true);
+    
+  // スタート
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(3);
+  
+  // タイトル
+  M5.Lcd.setCursor(10, 80);
+  M5.Lcd.println("QR code + Button");
 }
-
+  
 void loop() {
-}
-
-void displayQRCode(String data) {
-  QRCode qrcode;
-  uint8_t qrcodeData[qrcode_getBufferSize(3)];
-  qrcode_initText(&qrcode, qrcodeData, 3, ECC_LOW, data.c_str());
-
-  uint32_t qrSize = qrcode.size * 5; // QRコードの拡大倍率
-  uint32_t x = (M5.Lcd.width() - qrSize) / 2;
-  uint32_t y = (M5.Lcd.height() - qrSize) / 2;
-
-  M5.Lcd.fillRect(x-3, y-3, qrSize+6, qrSize+6, TFT_BLACK);
-  M5.Lcd.drawRect(x-3, y-3, qrSize+6, qrSize+6, TFT_WHITE);
-
-  for (uint8_t i = 0; i < qrcode.size; i++) {
-    for (uint8_t j = 0; j < qrcode.size; j++) {
-      if (qrcode_getModule(&qrcode, i, j)) {
-        M5.Lcd.fillRect(x + i * 5, y + j * 5, 5, 5, TFT_BLACK);
-      } else {
-        M5.Lcd.fillRect(x + i * 5, y + j * 5, 5, 5, TFT_WHITE);
-      }
-    }
+  M5.update();
+   
+  if (M5.BtnA.wasReleased()) {
+    // A ボタン
+    // https://1ft-seabass.jp/ のQRコードを表示
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.qrcode("https://1ft-seabass.jp/",50,10,220,5);
+  } else if (M5.BtnB.wasReleased()) {
+    // B ボタン
+    // LINE BOT の友達追加 URL の QR コードを表示
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.qrcode("https://line.me/R/ti/p/@XXXXXXXXXX",50,10,220,5);
+  } else if (M5.BtnC.wasReleased()) {
+    // C ボタン
+    // Twitter URL https://twitter.com/1ft_seabass のQRコードを表示
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.qrcode("https://twitter.com/1ft_seabass",50,10,220,5);
   }
 }
